723868e227c41921d76232180d55553f5da78635
Dania2021 <dania.khanam@gmail.com>
2023-01-05 17:04:01 +0000

    Add class Recipe and class Comment in models.py

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,93 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+STATUS = (('0', 'Pending'), ('1', 'Approved'), ('2', 'Denied'))
+
+
+class Recipe(models.Model):
+    '''
+    To create recipe where user can add a recipe
+    '''
+    AMERICAN = 'American'
+    BRITISH = 'British'
+    CHINESE = 'Chinese'
+    EUROPEAN = 'European'
+    FRENCH = 'French'
+    GERMAN = 'German'
+    GREEK = 'Greek'
+    INDIAN = 'Indian'
+    INDONESIAN = 'Indonesian'
+    IRISH = 'Irish'
+    ITALIAN = 'Italian'
+    JAPANESE = 'Japanese'
+    KOREAN = 'Korean'
+    MEXICAN = 'Mexican'
+    SPANISH = 'Spanish'
+    THAI = 'Thai'
+    TURKISH = 'Turkish'
+
+    CUISINE_CHOICES = [
+        (AMERICAN, 'American'),
+        (BRITISH, 'British'),
+        (CHINESE, 'Chinese'),
+        (EUROPEAN, 'European'),
+        (FRENCH, 'French'),
+        (GERMAN, 'German'),
+        (GREEK, 'Greek'),
+        (INDIAN, 'Indian'),
+        (INDONESIAN, 'Indonesian'),
+        (IRISH, 'Irish'),
+        (ITALIAN, 'Italian'),
+        (JAPANESE, 'Japanese'),
+        (KOREAN, 'Korean'),
+        (MEXICAN, 'Mexican'),
+        (SPANISH, 'Spanish'),
+        (THAI, 'Thai'),
+        (TURKISH, 'Turkish')
+    ]
+
+    title = models.CharField(max_length=200, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='recipe_posts')
+    ingredients = models.TextField()
+    recipe_image = CloudinaryField('image', default='placeholder')
+    steps = models.TextField()
+    serves = models.IntegerField()
+    duration = models.IntegerField()
+    status = models.IntegerField(choices=STATUS, default=0)
+    cuisine_name = models.CharField(
+        max_length=30, choices=CUISINE_CHOICES, default=AMERICAN)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        '''
+        order recipe by newest first
+        '''
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    '''
+    To leave comments on recipes
+    '''
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, null=True, blank=True)
+    recipe = models.ForeignKey(
+        Recipe, on_delete=models.CASCADE, related_name='comments')
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        '''
+        order comment by oldest first
+        '''
+        ordering = ['created_on']
+
+    def __str__(self):
+        return f'Comment by {self.author} on Recipe {self.recipe}'
