4349701f3a4ec42ef39d231abc402cbc667bb67c
Dania2021 <dania.khanam@gmail.com>
2023-01-06 00:15:59 +0000

    Add class RecipeAdmin in admin.py

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Recipe
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Recipe)
+class RecipeAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'author', 'status', 'created_on')
+    search_fields = ['cuisine_name', 'author']
+    list_filter = ('status', 'created_on')
+    summernote_fields = 'steps'
