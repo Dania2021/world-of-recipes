214150acd9eb11a49495a1390ff86ce6313f8cd0
Dania2021 <dania.khanam@gmail.com>
2023-01-07 00:53:16 +0000

    Add CommentAdmin class in admin.py

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Recipe
+from .models import Recipe, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
@@ -10,3 +10,15 @@ class RecipeAdmin(SummernoteModelAdmin):
     search_fields = ['cuisine_name', 'author']
     list_filter = ('status', 'created_on')
     summernote_fields = 'steps'
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+
+    list_display = ('author', 'body', 'created_on', 'recipe', 'approved')
+    search_fields = ['author', 'recipe']
+    list_filter = ('author',)
+    actions = ['comments_approved']
+
+    def comments_approved(self, request, queryset):
+        queryset.update(approved=True)
