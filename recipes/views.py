d0d712b891ececc88aa1707ab1c669c76378f9e4
Dania2021 <dania.khanam@gmail.com>
2023-01-14 01:51:45 +0000

    Update template_name of class RecipeList in views.py

--- recipes/views.py
+++ recipes/views.py
@@ -6,5 +6,5 @@ from .models import Recipe
 class RecipeList(generic.ListView):
     model = Recipe
     recipe_list = Recipe.objects.all().order_by('-created_on')
-    template_name = 'index.html'
+    template_name = 'recipes_list.html'
     paginate_by = 6
