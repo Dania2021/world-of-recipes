d0877a922c38ab6fd4099a7c4567806a72a1ca45
Dania2021 <dania.khanam@gmail.com>
2023-01-16 01:10:06 +0000

    Add url for home view in urls.py

--- recipes/urls.py
+++ recipes/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 
 
 urlpatterns = [
+    path('', views.Home.as_view(), name='home'),
     path(
         'recipes_list/',
         views.RecipeList.as_view(), name='recipes_list'),
