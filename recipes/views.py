784edb5e1c61b8ab493218455769afdc08eee9db
Dania2021 <dania.khanam@gmail.com>
2023-01-17 15:52:11 +0000

    Added class RecipeDetail in views.py

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from .models import Recipe
 
@@ -16,4 +16,22 @@ class Home(View):
         return render(
             request,
             'index.html'
+        )
+
+
+class RecipeDetail(View):
+    model = Recipe
+    fields = '__all__'
+
+    def get(self, request, author_id, *args, **kwargs):
+        recipe_detail = Recipe.objects.all()
+        recipe = get_object_or_404(recipe_detail, id=author_id)
+        comments = recipe.comments.filter(approved=True)
+        return render(
+            request,
+            'recipe_detail.html',
+            {
+                'recipe': recipe,
+                'comments': comments
+            },
         )
