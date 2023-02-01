b188a2f7e2c2a068b16f18db195118578daec2e9
Dania2021 <dania.khanam@gmail.com>
2023-02-01 02:00:01 +0000

    Add code to connect receiver in ready method

--- recipes/apps.py
+++ recipes/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class RecipesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'recipes'
+
+    # to connect the receiver in ready method
+    def ready(self):
+        import recipes.signals
